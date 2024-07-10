a9d4fbafa79387f5cea720edf8b4be5aee6a601d
CrimsonRuby11 <skprithviraj8@gmail.com>
2024-07-10 13:21:12 +0530

    Cleanup, Comments

--- Assets/Scripts/Grid/GridController.cs
+++ Assets/Scripts/Grid/GridController.cs
@@ -100,17 +100,21 @@ public class GridController : MonoBehaviour
     }
 
     bool canVisit(int x, int y) {
-        // if cube exists, and cube is not an obstacle, and cube's not visited.
-        return cubes[x, y] && !cubes[x, y].isObstacle && !cubes[x, y].visited;
+        // if cube exists, and cube is not an obstacle
+        return cubes[x, y] && !cubes[x, y].isObstacle;
     }
 
     public List<CubeController> pathFind(CubeController startingCube, int endX, int endY) {
 
+        // A* PATHFINDING
+        // cache endPoint
         CubeController endCube = cubes[endX, endY];
 
+        // Two lists to keep track of visited cubes and unvisited ones
         List<CubeController> openList = new List<CubeController>() {startingCube};
         List<CubeController> closedList = new List<CubeController>();
 
+        // Reset all cubes to not visited
         for(int x = 0; x < 10; x++) {
             for(int y = 0; y < 10; y++) {
                 cubes[x, y].gCost = int.MaxValue;
@@ -119,30 +123,46 @@ public class GridController : MonoBehaviour
             }
         }
 
+        // Initialize starting position with gCost = 0, hCost according to endCube
         startingCube.gCost = 0;
         startingCube.hCost = calculateDistanceCost(startingCube, endCube);
         startingCube.calculateFCost();
 
+        // If openList contains nodes
         while(openList.Count > 0) {
+
+            // Get element with lowest fCost and check if its the end point (if reached destination)
             CubeController currentCube = getLowestF(openList);
             if(currentCube == endCube) {
+                // If reached, return the final path
                 return calculatePath(endCube);
             }
 
+            // Mark current cube as visited, remove from open list and add to closed
             openList.Remove(currentCube);
             closedList.Add(currentCube);
 
+            // For each neighbour of currentCube
             foreach(CubeController c in getNeighbours(currentCube)) {
+                // Ignore if neighbour already visited
                 if(closedList.Contains(c)) continue;
 
+                // Get gCost according to currentCube's gCost and new distance
                 float newG = currentCube.gCost + calculateDistanceCost(currentCube, c);
+
+                // Get the neighbour that we didnt already visit. those whose gCost is more than that of the currentCube
                 if(newG < c.gCost) {
+
+                    // Set currentCube as parent to new cube
                     c.previousCube = currentCube;
+
+                    // Update gCost, hCost, and fCost
                     c.gCost = newG;
                     c.hCost = calculateDistanceCost(c, endCube);
                     c.calculateFCost();
                 }
 
+                // If neighbour wasn't already searched by other cubes, add to openList
                 if(!openList.Contains(c)) {
                     openList.Add(c);
                 }
@@ -153,28 +173,39 @@ public class GridController : MonoBehaviour
     }
 
     private List<CubeController> getNeighbours(CubeController currentCube) {
+        // Initiate list
         List<CubeController> neighbours = new List<CubeController>();
 
+        // Cache position
         int x = currentCube.gridPosition.x;
         int y = currentCube.gridPosition.z;
 
+        // Left
         if(x-1 > -1 && canVisit(x-1, y)) neighbours.Add(cubes[x-1, y]);
+        // Right
         if(x+1 < 10 && canVisit(x+1, y)) neighbours.Add(cubes[x+1, y]);
+        // Bottom
         if(y-1 > -1 && canVisit(x, y-1)) neighbours.Add(cubes[x, y-1]);
+        // Top
         if(y+1 < 10 && canVisit(x, y+1)) neighbours.Add(cubes[x, y+1]);
 
         return neighbours;
     }
 
     private List<CubeController> calculatePath(CubeController endCube) {
+        // Initiate Path
         List<CubeController> path = new List<CubeController>() {endCube};
 
+        // Start from the endCube
         CubeController currentCube = endCube;
+
+        // If currentCube's parent exists, add to path and set currentCube to parent
         while(currentCube.previousCube != null) {
             path.Add(currentCube.previousCube);
             currentCube = currentCube.previousCube;
         }
 
+        // Reverse the path and return
         path.Reverse();
         return path;
     }
@@ -191,9 +222,14 @@ public class GridController : MonoBehaviour
     }
 
     private int calculateDistanceCost(CubeController a, CubeController b) {
+        // calculate linear distances along x and y
         int x = Mathf.Abs(a.gridPosition.x - b.gridPosition.x);
         int y = Mathf.Abs(a.gridPosition.z - b.gridPosition.z);
+
+        // Calculate diagonal distances
         int remaining = Mathf.Abs(x - y);
+
+        // Return total distance
         return 14*Mathf.Min(x, y) + 10*remaining;
     }
 
